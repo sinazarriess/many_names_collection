6b66f46af0692798ae119a8d3eb0370d00f41a2c
Carina <carina.silberer@upf.edu>
2018-04-02 14:26:28 +0200

    adds scripts for annotating refExprs with attributes, pos tags, dep parses and synsets

---
+++ utils/df_annotation.py
@@ -0,0 +1,83 @@
+
+import os
+import sys
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import annotation_utils as anno_utils
+
+USRNAME ='u148188' # 'carina'
+
+def load_df(fpath):
+    return pd.read_json(fpath, compression='gzip', orient='split')
+
+# Stanford PoS Tagger
+def add_pos_tags(refdf, out_fpath=None):
+    pos_tagger = anno_utils.load_pos_tagger()
+    refdf['tagged_stnf'] = anno_utils.tag_refExp(refdf[['refexp']].values.flatten(order='K').tolist(), pos_tagger=pos_tagger)
+    if out_fpath:
+        refdf.to_json(out_fpath, compression='gzip', orient='split')
+    return refdf
+
+def add_dep_parses(refdf, out_fpath=None):
+    dep_parser = anno_utils.load_dep_parser()    
+    parses = anno_utils.parse_refEpx(
+        refdf[['refexp']].values.flatten(order='K').tolist(), dep_parser)
+    refdf['depparse_stnf'] = parses
+    if out_fpath:
+        refdf.to_json(out_fpath, compression='gzip', orient='split')
+    return refdf
+
+# Attributes and Names
+def add_attrs_names(refdf, out_fpath=None):
+    if 'tagged' in refdf:
+        refdf['attr_name'] = refdf['tagged'].apply(lambda x: anno_utils.get_refanno(x))
+    if 'tagged_stnf' in refdf:
+        refdf['attr_name_stnf'] = refdf['tagged_stnf'].apply(lambda x: anno_utils.get_refanno(x))
+    if out_fpath:
+        refdf.to_json(out_fpath, compression='gzip', orient='split')
+    return refdf
+    
+
+# WordNet
+def add_synsets(refdf, out_fpath=None):
+    if 'tagged' in refdf:
+        refdf['wn_anno'] = refdf['tagged'].apply(lambda x: get_wn_anno(x))
+    if 'tagged_stnf' in refdf:
+        refdf['wn_anno_stnf'] = refdf['tagged_stnf'].apply(lambda x: get_wn_anno(x))
+    if out_fpath:
+        refdf.to_json(out_fpath, compression='gzip', orient='split')
+    return refdf
+    
+def get_wn_anno(refdf_tagged):
+    wn_annos = []
+    for (word, tag) in refdf_tagged:
+        pos = anno_utils.tag2pos(tag)
+        if pos:
+            synset = anno_utils.get_synset_first(word, pos=pos)
+            lexfile_info = anno_utils.get_ss_lexfile_info(synset)
+            wn_annos.append((anno_utils.get_synset_name(synset), lexfile_info))
+        else:
+            wn_annos.append((None, None))
+    return wn_annos
+
+if __name__=="__main__":
+    json_fpath = "/media/%s/Carina_2017/UdS/data/flickr30k_refdf.json.gz" % (USRNAME)
+    json_foutpath = "/media/%s/Carina_2017/UdS/data/flickr30k_refdf_wn.json.gz" % (USRNAME)
+
+    if len(sys.argv) > 1:
+        json_fpath = sys.argv[1]
+        if len(sys.argv) > 2:
+            json_foutpath = sys.argv[2]
+
+    refdf = load_df(json_fpath)
+    #refdf = add_pos_tags(refdf) # assignment not really necessary
+    #refdf = add_synsets(refdf)
+    #refdf = add_attrs_names(refdf, json_foutpath)
+    refdf = add_dep_parses(refdf)
+    
+    
+    
