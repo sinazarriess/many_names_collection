c59cf67f6ab2e9ce6a212c75f436eceb24ffcac6
carinasilberer <carinasilberer@gmail.com>
2019-02-26 18:46:36 +0100

    Add files via upload

--- analysis/load_results.py
+++ analysis/load_results.py
@@ -9,8 +9,32 @@ import numpy as np
 #from spellchecker import SpellChecker
 #from nltk.stem.wordnet import WordNetLemmatizer
 
+def load_resultsdata_csv(datafile_csv):
+    fulldf = pd.read_csv(datafile_csv, sep="\t")
+    fulldf[["responses"]] = fulldf["responses"].apply(lambda a: eval(a))
+    return fulldf
+
+def _img2objname_map(imgdf):
+    # meta file used to publish the hits
+    img2names_map = dict()
+    for (idx, row) in imgdf.iterrows():
+        img2names_map[str(row.image_id)+"_"+str(row.object_id)] = (row.obj_names, row.category)
+    return img2names_map
+
+def add_objnames_categories(fulldf, img2names_map):
+    fulldf["sample_type"] = fulldf["cat"]
+    obj_names = []
+    obj_cats = []
+    for (idx, row) in fulldf.iterrows():
+        name_cat = img2names_map.get(str(row.vg_img_id)+"_"+str(row.vg_object_id), ["UNK", "UNK"])
+        obj_names.append(name_cat[0])
+        obj_cats.append(name_cat[1])
+    fulldf["obj_name"] = obj_names
+    fulldf["cat"] = obj_cats
+    return fulldf
+
+
 def process_answers(resultfile, hit2images, with_opt_outs=False):
-    
     with open(resultfile, 'r') as handle:
         resultlist = json.load(handle)
     
@@ -19,8 +43,9 @@ def process_answers(resultfile, hit2images, with_opt_outs=False):
     
     for assignm in resultlist:
         hitid = assignm['HITId']
-        print(hitid)
+        #print(hitid)
         for a in assignm['Assignments']:
+            skip_assignment = False
             #print(a)
             workerId = a['WorkerId']
             answers = a['Answers']
@@ -35,30 +60,40 @@ def process_answers(resultfile, hit2images, with_opt_outs=False):
                     name = name.lower()
                     obj2answers[object_id][name] += 1
                 elif with_opt_outs:
-                    opt_out = "#"+answers[str(ix)]['optout-'+str(ix)]
-                    if answers[str(ix)]['other_reasons-'+str(ix)]:
-                        opt_out += "_" + answers[str(ix)]['other_reasons-'+str(ix)]
-                    obj2answers[object_id][opt_out] += 1
-                    
+                    try:
+                        opt_out = "#"+answers[str(ix)]['optout-'+str(ix)]
+                        if answers[str(ix)]['other_reasons-'+str(ix)]:
+                            opt_out += "_" + answers[str(ix)]['other_reasons-'+str(ix)].lower()
+                        obj2answers[object_id][opt_out] += 1
+                    except KeyError:
+                        # THIS SHOULD NOT HAPPEN!!
+                        sys.stderr.write("No input given for object %d (assignmentId %s)\n" % (ix, a['AssignmentId']))
+                        skip_assignment = True
+                        break
+            if skip_assignment:
+                sys.stderr.write("Skipping assignment with Id %s\n" % (a['AssignmentId']))
+                continue
                     
     ## optional: spell checking
     ## commented this out, as it didn't really seem to work
     ## obj2clean_answers = {objid:check_spelling(obj2answers[objid]) for objid in obj2answers}
 
-
-        
     allrows = [] 
     for objid in obj2answers:
         row = obj2info[objid]
         row.append(obj2answers[objid])
         allrows.append(row)
-        
-    fulldf = pd.DataFrame(allrows,columns=['vg_img_id','vg_object_id','cat','synset','url','responses'])
-                    
+    
+    if len(allrows[0]) == 8:
+        # new hits file (json), with category and object name
+        cols = ["vg_img_id", "vg_object_id", "sample_type", "synset", "obj_name", "cat", "url", "responses"]
+    else:
+        # old hits file (up to run 0f)
+        cols = ['vg_img_id','vg_object_id','cat','synset','url','responses']
+    fulldf = pd.DataFrame(allrows,columns=cols)
     return fulldf
                     
 def process_hits(hitfile):
-    
     with open(hitfile, 'r') as handle:
         hitlist = json.load(handle)
         
