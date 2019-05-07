08b98b5ca9ad544ac933769df1822c7d4b6ffe92
Sina Zarrieß <sina@dhcp-10-68-193-217.dhcp.uni-bielefeld.de>
2019-05-07 14:59:02 +0200

    2nd version of extracting naming variants

--- analysis/wordnet_analysis.py
+++ analysis/wordnet_analysis.py
@@ -29,6 +29,16 @@ def name_pairs(rdict):
 
     return np_count
 
+def vg_name_pairs(vg_name,rdict):
+
+    np_count = Counter()
+
+    for oname in rdict:
+        if not oname == vg_name:
+            np_count[(vg_name,oname)] = rdict[oname]
+
+    return np_count
+
 
 def is_hyponym(w1,w2,max_depth=5):
 
@@ -96,17 +106,18 @@ def is_cohyponym(w1,w2,max_depth=1):
 fn = 'all_responses_round0-3_cleaned.csv'
 resdf = make_df(fn)
 
-ordered_paircount = Counter()
+#ordered_paircount = Counter()
 paircount = Counter()
 wordcount = Counter()
 
-for ndict in resdf['spellchecked']:
+for x,row in resdf.iterrows():
     #print(ndict)
-    x = name_pairs(ndict)
-    ordered_paircount.update(x)
-    for pair in x:
-        paircount[tuple(sorted(pair))] += x[pair]
-    wordcount.update(ndict)
+    #x = name_pairs(ndict)
+    x = vg_name_pairs(row['vg_obj_name'],row['spellchecked'])
+    paircount.update(x)
+    #for pair in x:
+    #    paircount[tuple(sorted(pair))] += x[pair]
+    wordcount.update(row['spellchecked'])
 
 print("N name types:",len(wordcount))
 singletons = [w for w in wordcount if wordcount[w] == 1]
@@ -119,13 +130,13 @@ for w in wordcount:
         wn_wordcount[w] = wordcount[w]
         wn_map[w] = w
     elif len(wn.synsets('_'.join(w.split(' ')))) > 0:
-        wn_wordcount['_'.join(w.split(' '))] = wordcount[w]
+        wn_wordcount[w] = wordcount[w]
         wn_map[w] = '_'.join(w.split(' '))
     elif len(wn.synsets(''.join(w.split(' ')))) > 0:
-        wn_wordcount[''.join(w.split(' '))] = wordcount[w]
+        wn_wordcount[w] = wordcount[w]
         wn_map[w] = ''.join(w.split(' '))
     elif len(wn.synsets('-'.join(w.split(' ')))) > 0:
-        wn_wordcount['-'.join(w.split(' '))] = wordcount[w]
+        wn_wordcount[w] = wordcount[w]
         wn_map[w] = '-'.join(w.split(' '))
 
 wn_singletons = [w for w in wn_wordcount if wn_wordcount[w] == 1]
@@ -135,43 +146,51 @@ print("... singletons:",len(wn_singletons))
 
 wn_paircount = Counter({(w1,w2):paircount[(w1,w2)] for (w1,w2) in paircount if w1 in wn_wordcount \
                                                                                and w2 in wn_wordcount })
-wn_ordered_paircount = Counter({(w1,w2):ordered_paircount[(w1,w2)] for (w1,w2) in ordered_paircount if w1 in wn_wordcount and w2 in wn_wordcount })
+#wn_ordered_paircount = Counter({(w1,w2):ordered_paircount[(w1,w2)] for (w1,w2) in ordered_paircount if w1 in wn_wordcount and w2 in wn_wordcount })
 
-print("N ordered name variants:",len(ordered_paircount))
-print("N unordered name variants :",len(paircount))
-print("N ordered name variants covered by WN:",len(wn_ordered_paircount))
-singletons = [w for w in wn_ordered_paircount if wn_ordered_paircount[w] == 1]
-print("... singletons:",len(singletons))
-print("N unordered name variants covered by WN:",len(wn_paircount))
+#print("N ordered name variants:",len(ordered_paircount))
+#print("N name variants :",len(paircount))
+#print("N ordered name variants covered by WN:",len(wn_ordered_paircount))
+#singletons = [w for w in wn_ordered_paircount if wn_ordered_paircount[w] == 1]
+#print("... singletons:",len(singletons))
+print("N name variants covered by WN:",len(wn_paircount))
 singletons = [w for w in wn_paircount if wn_paircount[w] == 1]
 print("... singletons:",len(singletons))
 
 
 
-wn_pairrel = {}
+pairrel = {}
 
-for (top,other) in wn_paircount:
+for (top,other) in paircount:
 
-    if is_synonym(top,other):
-        wn_pairrel[(top,other)] = 'synonymy'
-    elif is_hypernym(top,other,max_depth=10):
-        wn_pairrel[(top,other)] = 'hypernymy.1'
-    elif is_hypernym(other,top,max_depth=10):
-        wn_pairrel[(top,other)] = 'hypernymy.2'
-    elif is_cohyponym(top,other,max_depth=1):
-        wn_pairrel[(top,other)] = 'co-hyponymy'
+
+    if (top,other) in wn_paircount:
+        if is_synonym(wn_map[top],wn_map[other]):
+            pairrel[(top,other)] = 'synonymy'
+        elif is_hypernym(wn_map[top],wn_map[other],max_depth=10):
+            pairrel[(top,other)] = 'hypernymy.1'
+        elif is_hypernym(wn_map[other],wn_map[top],max_depth=10):
+            pairrel[(top,other)] = 'hypernymy.2'
+        elif is_cohyponym(wn_map[top],wn_map[other],max_depth=1):
+            pairrel[(top,other)] = 'co-hyponymy'
+        else:
+            pairrel[(top,other)] = 'crossclassified'
     else:
-        wn_pairrel[(top,other)] = 'crossclassified'
+        pairrel[(top,other)] = 'not-covered'
+
+
+print("pairrel",len(pairrel))
+print("paircount",len(paircount))
 
 
 typecounts = Counter()
 tokencounts = Counter()
-for p in wn_pairrel:
-    rel = wn_pairrel[p]
+for p in pairrel:
+    rel = pairrel[p]
     if '.' in rel:
         rel = rel[:-2]
     typecounts[rel] += 1
-    tokencounts[rel] += wn_paircount[p]
+    tokencounts[rel] += paircount[p]
 
 
 outdf = []
@@ -188,16 +207,14 @@ print(outdff.sort_values(by=['types']).to_latex(index=False))
 
 pairdf = []
 
-for (top,other) in wn_paircount:
-    prow = (top,other,wn_pairrel[(top,other)],\
-           wn_paircount[(top,other)],\
-           wn_ordered_paircount[(top,other)],\
-           wn_ordered_paircount[(other,top)])
+for (top,other) in paircount:
+    prow = (top,other,pairrel[(top,other)],\
+           paircount[(top,other)])
     pairdf.append(prow)
 
-pairdf = pd.DataFrame(pairdf,columns=['word1','word2','relation','freq. (unord)','freq. (w1,w2)','freq. (w2,w1)'])
-pairdf = pairdf.sort_values(by=['freq. (unord)'],ascending=False)
-pairdf.to_csv("names_pairs_relations.csv")
+pairdf = pd.DataFrame(pairdf,columns=['word1','word2','relation','freq.'])
+pairdf = pairdf.sort_values(by=['freq.'],ascending=False)
+pairdf.to_csv("names_pairs_relations_v2.csv")
 
 
 
