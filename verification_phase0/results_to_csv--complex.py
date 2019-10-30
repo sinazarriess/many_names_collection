3da7860bcd70dae2daff6bc766389b4f2803a39b
matthijswestera <matthijs.westera@gmail.com>
2019-10-30 22:39:11 +0100

    ignore control items without majority correct; add decision column (approve/reject/bonus/block); also minor tweaks/refractoring

--- verification_phase0/results_to_csv--complex.py
+++ verification_phase0/results_to_csv--complex.py
@@ -1,4 +1,5 @@
 import pandas as pd
+pd.options.display.max_colwidth = 100
 import json
 import glob
 
@@ -6,16 +7,19 @@ import numpy as np
 
 import os
 
+
 ANONYMIZE = False
+CONTROL_RELIABILITY_THRESHOLD = .5
+APPROVAL_TRESHOLD = .7  # TODO Can be higher once I have more annotations?
+BONUS_THRESHOLD = 1.0
+BLOCK_THRESHOLD = .8
 
-pd.options.display.max_colwidth = 100
 # TODO Generalize; get paths from a config argument?
-
 resultsdir = '1_pre-pilot/results/complex'
 
 
 # Read all assignments from the .json file from MTurk
-assignments = []
+assignments_from_mturk = []
 for filename in glob.glob(os.path.join(resultsdir, '*.json')):
     with open(filename) as file:
         data = json.load(file)
@@ -29,13 +33,13 @@ for filename in glob.glob(os.path.join(resultsdir, '*.json')):
                     answers.update(assignment['Answers'][key])
                 assignment.update(answers)
                 del assignment['Answers']
-                assignments.append(assignment)
+                assignments_from_mturk.append(assignment)
 
                 # TODO Get RequesterAnnotation
 
-assignments = pd.DataFrame(assignments)
-assignments.columns = [x.lower() for x in assignments.columns]
-assignments.rename(columns={"stored_font_size": 'control_score_mturk'}, inplace=True)
+assignments_from_mturk = pd.DataFrame(assignments_from_mturk)
+assignments_from_mturk.columns = [x.lower() for x in assignments_from_mturk.columns]
+assignments_from_mturk.rename(columns={"stored_font_size": 'control_score_mturk'}, inplace=True)
 
 MAX_N_IMAGES = 11
 MAX_N_NAMES = 16
@@ -43,35 +47,35 @@ MAX_N_NAMES = 16
 # Merge boolean radiobutton columns into integer-valued rating columns
 for i in range(MAX_N_IMAGES):
     for n in range(MAX_N_NAMES):
-        assignments['img{}-name{}-rating'.format(i,n)] = np.nan
-        assignments['img{}-name{}-type'.format(i, n)] = np.nan
-        assignments['img{}-name{}-color'.format(i, n)] = np.nan
-for id, assignment in assignments.iterrows():
+        assignments_from_mturk['img{}-name{}-rating'.format(i, n)] = np.nan
+        assignments_from_mturk['img{}-name{}-type'.format(i, n)] = np.nan
+        assignments_from_mturk['img{}-name{}-color'.format(i, n)] = np.nan
+for id, assignment in assignments_from_mturk.iterrows():
     for i in range(MAX_N_IMAGES):
         for n in range(MAX_N_NAMES):
             for r in range(3):
-                if 'img{}-name{}-rating.{}'.format(i,n,r) in assignments and assignment['img{}-name{}-rating.{}'.format(i,n,r)] == 'true':
-                    assignments.at[id, 'img{}-name{}-rating'.format(i,n)] = r
+                if 'img{}-name{}-rating.{}'.format(i,n,r) in assignments_from_mturk and assignment['img{}-name{}-rating.{}'.format(i, n, r)] == 'true':
+                    assignments_from_mturk.at[id, 'img{}-name{}-rating'.format(i, n)] = r
             for r in range(4):
-                if 'img{}-name{}-type.{}'.format(i,n,r) in assignments and assignment['img{}-name{}-type.{}'.format(i,n,r)] == 'true':
-                    assignments.at[id, 'img{}-name{}-type'.format(i,n)] = r
+                if 'img{}-name{}-type.{}'.format(i,n,r) in assignments_from_mturk and assignment['img{}-name{}-type.{}'.format(i, n, r)] == 'true':
+                    assignments_from_mturk.at[id, 'img{}-name{}-type'.format(i, n)] = r
             for r in list(range(MAX_N_NAMES)) + ['x']:
-                if 'img{}-name{}-color.{}'.format(i,n,r) in assignments and assignment['img{}-name{}-color.{}'.format(i,n,r)] == 'true':
-                    assignments.at[id, 'img{}-name{}-color'.format(i,n)] = -1 if r == 'x' else r
+                if 'img{}-name{}-color.{}'.format(i,n,r) in assignments_from_mturk and assignment['img{}-name{}-color.{}'.format(i, n, r)] == 'true':
+                    assignments_from_mturk.at[id, 'img{}-name{}-color'.format(i, n)] = -1 if r == 'x' else r
 for i in range(MAX_N_IMAGES):
     for n in range(MAX_N_NAMES):
         for r in range(3):
-            if 'img{}-name{}-rating.{}'.format(i, n, r) in assignments:
-                del assignments['img{}-name{}-rating.{}'.format(i,n, r)]
+            if 'img{}-name{}-rating.{}'.format(i, n, r) in assignments_from_mturk:
+                del assignments_from_mturk['img{}-name{}-rating.{}'.format(i, n, r)]
         for r in range(4):
-            if 'img{}-name{}-type.{}'.format(i, n, r) in assignments:
-                del assignments['img{}-name{}-type.{}'.format(i, n, r)]
+            if 'img{}-name{}-type.{}'.format(i, n, r) in assignments_from_mturk:
+                del assignments_from_mturk['img{}-name{}-type.{}'.format(i, n, r)]
         for r in list(range(MAX_N_NAMES)) + ['x']:
-            if 'img{}-name{}-color.{}'.format(i, n, r) in assignments:
-                del assignments['img{}-name{}-color.{}'.format(i, n, r)]
+            if 'img{}-name{}-color.{}'.format(i, n, r) in assignments_from_mturk:
+                del assignments_from_mturk['img{}-name{}-color.{}'.format(i, n, r)]
 
 
-print(assignments[:5].to_string())
+print(assignments_from_mturk[:5].to_string())
 
 
 
@@ -79,12 +83,12 @@ print(assignments[:5].to_string())
 meta = ['assignmentid', 'hitid', 'workerid']
 dfs_per_image = []
 for i in reversed(range(MAX_N_IMAGES)):
-    columns = meta + [col for col in assignments.columns if (col == 'image_url_{}'.format(i) or
-                                                            col.startswith('name_{}_'.format(i)) or
-                                                            col == 'quality_control_{}'.format(i) or
-                                                            col.startswith('img{}-name'.format(i))) or
-                                                            col == 'img{}-comment'.format(i)]
-    df = assignments[columns]
+    columns = meta + [col for col in assignments_from_mturk.columns if (col == 'image_url_{}'.format(i) or
+                                                                        col.startswith('name_{}_'.format(i)) or
+                                                                        col == 'quality_control_{}'.format(i) or
+                                                                        col.startswith('img{}-name'.format(i))) or
+                      col == 'img{}-comment'.format(i)]
+    df = assignments_from_mturk[columns]
     df.columns = [col.replace('image_url_{}'.format(i), 'image_url').replace('name_{}_'.format(i), 'name_').replace('img{}-'.format(i), '').replace('quality_control_{}'.format(i), 'quality_control').lower() for col in df.columns]
     dfs_per_image.append(df)
 
@@ -114,30 +118,30 @@ for n in reversed(range(MAX_N_NAMES)):
         df = df.loc[df['name'] != '']
         dfs_per_name.append(df)
 
-per_name = pd.concat(dfs_per_name)
-per_name.reset_index(inplace=True)
+annotations = pd.concat(dfs_per_name)
+annotations.reset_index(inplace=True)
 
-per_name = per_name[['hitid', 'assignmentid', 'workerid', 'image', 'object', 'name', 'rating', 'type', 'color', 'image_url', 'quality_control']]
+annotations = annotations[['hitid', 'assignmentid', 'workerid', 'image', 'object', 'name', 'rating', 'type', 'color', 'image_url', 'quality_control']]
 
-per_name['rating'] = per_name['rating'].astype(int)
-per_name['type'] = per_name['type'].replace({0: 'linguistic', 1: 'bounding box', 2: 'visual', 3: 'other'})
-per_name['color'] = per_name['color'].astype(int)
+annotations['rating'] = annotations['rating'].astype(int)
+annotations['type'] = annotations['type'].replace({0: 'linguistic', 1: 'bounding box', 2: 'visual', 3: 'other'})
+annotations['color'] = annotations['color'].astype(int)
 
 
 # from 'color' column, compute list of same-colored names
-per_name['same_color'] = [[] for _ in range(len(per_name))]
-for i, row in per_name.iterrows():
+annotations['same_color'] = [[] for _ in range(len(annotations))]
+for i, row in annotations.iterrows():
     if row['color'] != -1:
-        same_color = per_name.loc[(per_name['assignmentid'] == row['assignmentid']) & (per_name['image'] == row['image']) & (per_name['object'] == row['object']) & (per_name['color'] == row['color'])]
+        same_color = annotations.loc[(annotations['assignmentid'] == row['assignmentid']) & (annotations['image'] == row['image']) & (annotations['object'] == row['object']) & (annotations['color'] == row['color'])]
         same_colored_names = same_color['name'].unique().tolist()
-        per_name.at[i, 'same_color'] = same_colored_names
+        annotations.at[i, 'same_color'] = same_colored_names
     else:
-        per_name.at[i, 'same_color'] = [row['name']]
+        annotations.at[i, 'same_color'] = [row['name']]
 
 # Un-obfuscate quality control; create 'control_type' column storing what type of quality control it is
-per_name['quality_control'] = per_name['quality_control'].apply(lambda x: json.loads(bytes.fromhex(x).decode('utf-8')))
-per_name['control_type'] = ""
-for i, row in per_name.iterrows():
+annotations['quality_control'] = annotations['quality_control'].apply(lambda x: json.loads(bytes.fromhex(x).decode('utf-8')))
+annotations['control_type'] = ""
+for i, row in annotations.iterrows():
     if row['name'] in row['quality_control']:
         item = row['quality_control'][row['name']].replace("sans-", "typo-")
         if item == "arial":
@@ -148,93 +152,114 @@ for i, row in per_name.iterrows():
             item = "random"
     else:
         item = ""
-    per_name.at[i,'control_type'] = item
+    annotations.at[i, 'control_type'] = item
 
-del per_name['quality_control']
+del annotations['quality_control']
 
 # Check if control items are correct
-per_name['correct1'] = np.nan
-per_name['correct2'] = np.nan
+annotations['correct1'] = np.nan
+annotations['correct2'] = np.nan
 # per_name['correct1'] = per_name['correct1'].astype(bool)    # to convert the Nones to proper nans?
 # per_name['correct2'] = per_name['correct2'].astype(bool)
-for i, row in per_name.iterrows():
+for i, row in annotations.iterrows():
     if row['control_type'] == 'vg_majority':
-        per_name.at[i, 'correct1'] = float(row['rating'] == 0)
+        annotations.at[i, 'correct1'] = float(row['rating'] == 0)
 
     elif row['control_type'].startswith('typo'):
         original = '-'.join(row['control_type'].split('-')[1:])
-        original_row = per_name.loc[per_name['assignmentid'] == row['assignmentid']].loc[(per_name['image'] == row['image']) & (per_name['object'] == row['object'])].loc[per_name['name'] == original].squeeze()
+        original_row = annotations.loc[annotations['assignmentid'] == row['assignmentid']].loc[(annotations['image'] == row['image']) & (annotations['object'] == row['object'])].loc[annotations['name'] == original].squeeze()
         if row['rating'] == 0:
-            per_name.at[i, 'correct1'] = float(False)
+            annotations.at[i, 'correct1'] = float(False)
         elif row['type'] != 'linguistic':
             if original_row['rating'] == 0:
-                per_name.at[i, 'correct1'] = float(False)
+                annotations.at[i, 'correct1'] = float(False)
             elif row['type'] != original_row['type']:
-                per_name.at[i, 'correct1'] = float(False)
+                annotations.at[i, 'correct1'] = float(False)
         else:
-            per_name.at[i, 'correct1'] = float(True)
-        per_name.at[i, 'correct2'] = float(original in row['same_color'])
+            annotations.at[i, 'correct1'] = float(True)
+        annotations.at[i, 'correct2'] = float(original in row['same_color'])
 
     elif row['control_type'] == 'alternative':
-        per_name.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] not in ['bounding box', 'other']))
-        positive = per_name.loc[(per_name['assignmentid'] == row['assignmentid']) & (per_name['image'] == row['image']) & (per_name['object'] == row['object']) & (per_name['control_type'] == 'vg_majority')]
+        annotations.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] not in ['bounding box', 'other']))
+        positive = annotations.loc[(annotations['assignmentid'] == row['assignmentid']) & (annotations['image'] == row['image']) & (annotations['object'] == row['object']) & (annotations['control_type'] == 'vg_majority')]
         if len(positive) > 0:
-            per_name.at[i, 'correct2'] = float(positive['name'].squeeze() not in row['same_color'])
+            annotations.at[i, 'correct2'] = float(positive['name'].squeeze() not in row['same_color'])
 
     elif row['control_type'] == 'random':
-        per_name.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] != 'other'))
-        per_name.at[i, 'correct2'] = float(len(row['same_color']) == 1)
+        annotations.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] != 'other'))
+        annotations.at[i, 'correct2'] = float(len(row['same_color']) == 1)
 
 
-per_name = per_name.sort_values(by=['workerid', 'hitid']).reset_index(drop=True)
+annotations = annotations.sort_values(by=['workerid', 'hitid']).reset_index(drop=True)
 
-per_name_mean = per_name.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean'})
-# TODO compute average performance on control items;
-# TODO exclude controls with low average performance
-# TODO compute new quality scores
+scores_per_name = annotations.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean'}).reset_index()
+
+# Compute which controls are reliable (>X% correct)
+reliable_controls1 = scores_per_name.loc[scores_per_name['correct1'] > CONTROL_RELIABILITY_THRESHOLD][['image', 'object', 'name']].values.tolist()
+reliable_controls2 = scores_per_name.loc[scores_per_name['correct2'] > CONTROL_RELIABILITY_THRESHOLD][['image', 'object', 'name']].values.tolist()
+for i, row in annotations.iterrows():
+    if [row['image'], row['object'], row['name']] not in reliable_controls1:
+        annotations.at[i, 'correct1'] = np.nan
+    if [row['image'], row['object'], row['name']] not in reliable_controls2:
+        annotations.at[i, 'correct2'] = np.nan
 
 
 print()
 
-per_name['rating'] = per_name['rating'].apply(lambda x: (2-x)/2)    # mapping 2 to 0, 1 to 1/2, 0 to 1.
+annotations['rating'] = annotations['rating'].apply(lambda x: (2 - x) / 2)    # mapping 2 to 0, 1 to 1/2, 0 to 1.
 
-print("Per name:", len(per_name))
-print(per_name[:10].to_string())
+print("Per name:", len(annotations))
+print(annotations[:10].to_string())
 
 
+assignments = annotations.groupby(['assignmentid', 'workerid']).agg({'rating': 'mean', 'color': 'mean', 'correct1': ['count', 'sum'], 'correct2': ['count', 'sum']})
+assignments.columns = ['_'.join(tup).rstrip('_') for tup in assignments.columns.values]
 
-per_assignment = per_name.groupby(['assignmentid', 'workerid']).agg({'rating': 'mean', 'correct1': ['count', 'sum'], 'correct2': ['count', 'sum']})
-per_assignment.columns = ['_'.join(tup).rstrip('_') for tup in per_assignment.columns.values]
+assignments['n_controls'] = assignments[['correct1_count', 'correct2_count']].sum(axis=1)
+assignments['control_score'] = (assignments['correct1_sum'] + assignments['correct2_sum']) / (assignments['correct1_count'] + assignments['correct2_count'])
 
-per_assignment['n_controls'] = per_assignment[['correct1_count', 'correct2_count']].sum(axis=1)
-per_assignment['control_score'] = (per_assignment['correct1_sum'] + per_assignment['correct2_sum']) / (per_assignment['correct1_count'] + per_assignment['correct2_count'])
+assignments = pd.merge(assignments, assignments_from_mturk[['assignmentid', 'control_score_mturk', 'workerid']], on='assignmentid')
+assignments['control_score_mturk'] = assignments['control_score_mturk'].apply(lambda x: 1 - float(x.split(',')[-1]))
 
-per_assignment = pd.merge(per_assignment, assignments[['assignmentid', 'control_score_mturk', 'workerid']], on='assignmentid')
-per_assignment['control_score_mturk'] = per_assignment['control_score_mturk'].apply(lambda x: 1-float(x.split(',')[-1]))
+del assignments['correct1_count']
+del assignments['correct1_sum']
+del assignments['correct2_count']
+del assignments['correct2_sum']
 
-del per_assignment['correct1_count']
-del per_assignment['correct1_sum']
-del per_assignment['correct2_count']
-del per_assignment['correct2_sum']
+assignments['decision1'] = assignments['control_score'].apply(lambda x: "approve" if x >= APPROVAL_TRESHOLD else "reject")
+assignments['decision2'] = assignments['control_score'].apply(lambda x: "bonus" if x >= BONUS_THRESHOLD else "block" if x < BLOCK_THRESHOLD else np.nan)
+assignments['executed1'] = False
+assignments['executed2'] = False
 
-per_assignment['decision'] = per_assignment['control_score'].apply(lambda x: "approve" if x > .5 else "reject")
-per_assignment['executed'] = False
+# Add column with reliable mistakes (URL + control_type + rating/type/samecolor)
+assignments['mistakes'] = [[] for _ in range(len(assignments))]
+for i, row in assignments.iterrows():
+    mistakes1 = annotations.loc[(annotations['assignmentid'] == row['assignmentid']) & (annotations['correct1'] == False)][['image_url', 'name', 'rating', 'type']].values.tolist()
+    mistakes2 = annotations.loc[(annotations['assignmentid'] == row['assignmentid']) & (annotations['correct2'] == False)][['image_url', 'name', 'same_color']].values.tolist()
+    mistakes1 = ["{} - {}: {}".format(m[0], m[1], m[2] if m[2] == 1 else m[3]) for m in mistakes1]
+    mistakes2 = ["{} - {}: {}".format(m[0], m[1], m[2]) for m in mistakes2]
+    assignments.at[i, 'mistakes'] = mistakes1 + mistakes2
+assignments['n_mistakes'] = assignments['mistakes'].apply(len)
 
-print(per_assignment.to_string())
+print(assignments.to_string())
 
 with open(os.path.join(resultsdir, 'assignments.csv'), 'w+') as outfile:
-    per_name.to_csv(outfile, index=False)
+    assignments.to_csv(outfile, index=False)
     print("Assignments written to", outfile.name)
 
 # TODO compare quality scores to those in the MTurk field (merge from the original per_HIT data).
 
-# TODO manual inspection
 
-quit()
+print("Total: {} assignments. Approve: {} (bonus: {}). Reject: {} (block: {})".format(len(assignments),
+                                                                                      len(assignments.loc[assignments['decision1'] == 'approve']),
+                                                                                      len(assignments.loc[assignments['decision2'] == 'bonus']),
+                                                                                      len(assignments.loc[assignments['decision1'] == 'reject']),
+                                                                                      len(assignments.loc[assignments['decision2'] == 'block']),
+                                                                                      ))
 
 print()
 
-print(per_name.groupby(['workerid', 'hitid'])['rating','correct1', 'correct2'].agg(['count', 'mean']).to_string())
+print(annotations.groupby(['workerid', 'hitid'])['rating', 'correct1', 'correct2'].agg(['count', 'mean']).to_string())
 
 # print(per_name.loc[per_name['control_type'] == 'alternative'].to_string())
 
@@ -242,13 +267,13 @@ print(per_name.groupby(['workerid', 'hitid'])['rating','correct1', 'correct2'].a
 # print(per_name.loc[per_name['control_type'].apply(lambda x: x.startswith('typo'))].to_string())
 
 if ANONYMIZE:
-    for i, workerid in enumerate(per_name['workerid'].unique()):
-        per_name.replace(to_replace={'workerid': {workerid: 'worker{}'.format(i)}}, inplace=True)
+    for i, workerid in enumerate(annotations['workerid'].unique()):
+        annotations.replace(to_replace={'workerid': {workerid: 'worker{}'.format(i)}}, inplace=True)
 
 # per_name['correct2'] = per_name['correct2'].astype(bool) ## TODO Meh this doesn't work.
 
 
 with open(os.path.join(resultsdir, 'names.csv'), 'w+') as outfile:
-    per_name.to_csv(outfile)
+    annotations.to_csv(outfile)
     print("Answers per name written to", outfile.name)
 
