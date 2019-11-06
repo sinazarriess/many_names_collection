107cbffae03e966c1fd2abfd20883b0ff3ebb662
matthijswestera <matthijs.westera@gmail.com>
2019-11-06 22:06:53 +0100

    some cleanup; allow inspection of unreliable controls

--- verification_phase0/results_to_csv--complex.py
+++ verification_phase0/results_to_csv--complex.py
@@ -14,6 +14,11 @@ APPROVAL_TRESHOLD = .7  # TODO Can be higher once I have more annotations?
 BONUS_THRESHOLD = 1.0
 BLOCK_THRESHOLD = .7
 
+INSPECT_FAILED_CONTROLS = False
+NO_REJECTION = True
+
+print("Warning: NO_REJECTION is set to true; all assignments will be accepted.")
+
 # TODO Generalize; get paths from a config argument?
 resultsdir = '1_pre-pilot/results/complex'
 
@@ -79,7 +84,6 @@ for i in range(MAX_N_IMAGES):
 print(assignments_from_mturk[:5].to_string())
 
 
-
 # Split by image
 meta = ['assignmentid', 'hitid', 'workerid']
 dfs_per_image = []
@@ -104,32 +108,32 @@ per_image['object'] = per_image['image_url'].apply(lambda x: x.split('//')[-1].s
 
 print("Per image:", len(per_image))
 
-print(per_image[:200].to_string())
 # print(per_image.groupby(['image', 'object']).agg({'workerid': lambda x: len(x.unique())}))
 
 
 
-# split by name
-dfs_per_name = []
+# Compile a dataframe with one row per annotation
+annotations = []
 for n in reversed(range(MAX_N_NAMES)):
     if 'name{}-rating'.format(n) in per_image:
         columns = ['assignmentid', 'hitid', 'workerid', 'image', 'object', 'name_{}'.format(n), 'name{}-rating'.format(n), 'name{}-type'.format(n), 'name{}-color'.format(n), 'image_url', 'quality_control']
         df = per_image[columns]
         df.columns = [col.replace('name_{}'.format(n), 'name').replace('name{}-'.format(n), '') for col in df.columns]
         df = df.loc[df['name'] != '']
-        dfs_per_name.append(df)
+        annotations.append(df)
 
-annotations = pd.concat(dfs_per_name)
+annotations = pd.concat(annotations)
+
+# Cleanup, typing, sorting
 annotations.reset_index(inplace=True)
-
 annotations = annotations[['hitid', 'assignmentid', 'workerid', 'image', 'object', 'name', 'rating', 'type', 'color', 'image_url', 'quality_control']]
-
 annotations['rating'] = annotations['rating'].astype(int)
 annotations['type'] = annotations['type'].replace({0: 'linguistic', 1: 'bounding box', 2: 'visual', 3: 'other'})
 annotations['color'] = annotations['color'].astype(int)
+annotations = annotations.sort_values(by=['workerid', 'hitid']).reset_index(drop=True)
 
 
-# from 'color' column, compute list of same-colored names
+# From 'color' column, compute list of same-colored names
 annotations['same_color'] = [[] for _ in range(len(annotations))]
 for i, row in annotations.iterrows():
     if row['color'] != -1:
@@ -138,6 +142,7 @@ for i, row in annotations.iterrows():
         annotations.at[i, 'same_color'] = same_colored_names
     else:
         annotations.at[i, 'same_color'] = [row['name']]
+
 
 # Un-obfuscate quality control; create 'control_type' column storing what type of quality control it is
 annotations['quality_control'] = annotations['quality_control'].apply(lambda x: json.loads(bytes.fromhex(x).decode('utf-8')))
@@ -157,11 +162,10 @@ for i, row in annotations.iterrows():
 
 del annotations['quality_control']
 
-# Check if control items are correct
+
+# Check if quality control items are correct
 annotations['correct1'] = np.nan
 annotations['correct2'] = np.nan
-# per_name['correct1'] = per_name['correct1'].astype(bool)    # to convert the Nones to proper nans?
-# per_name['correct2'] = per_name['correct2'].astype(bool)
 for i, row in annotations.iterrows():
     if row['control_type'] == 'vg_majority':
         annotations.at[i, 'correct1'] = float(row['rating'] == 0)
@@ -197,11 +201,14 @@ for i, row in annotations.iterrows():
         annotations.at[i, 'correct2'] = float(not any([syn not in row['same_color'] for syn in synonyms]))
 
 
-annotations = annotations.sort_values(by=['workerid', 'hitid']).reset_index(drop=True)
+# Change how rating is represented; this is a bit risky, but it works, so let's not touch it.
+annotations['rating'] = annotations['rating'].apply(lambda x: (2 - x) / 2)    # mapping 2 to 0, 1 to 1/2, 0 to 1.
 
-scores_per_name = annotations.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean'}).reset_index()
 
 # Compute which controls are reliable (>X% correct)
+scores_per_name = annotations.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean', 'image_url': lambda x: x.tolist()[0],  'control_type': lambda x: x.tolist()[0], 'rating': lambda x: x.tolist(), 'type': lambda x: x.tolist(), 'same_color': lambda x: x.tolist()}).reset_index()
+n_controls1 = sum(~scores_per_name['correct1'].isna())
+n_controls2 = sum(~scores_per_name['correct2'].isna())
 reliable_controls1 = scores_per_name.loc[scores_per_name['correct1'] > CONTROL_RELIABILITY_THRESHOLD][['image', 'object', 'name']].values.tolist()
 reliable_controls2 = scores_per_name.loc[scores_per_name['correct2'] > CONTROL_RELIABILITY_THRESHOLD][['image', 'object', 'name']].values.tolist()
 for i, row in annotations.iterrows():
@@ -210,35 +217,54 @@ for i, row in annotations.iterrows():
     if [row['image'], row['object'], row['name']] not in reliable_controls2:
         annotations.at[i, 'correct2'] = np.nan
 
+# Some summary stats
+print("Controls 1: {} (reliable: {}); controls 2: {} (reliable: {})".format(n_controls1, len(reliable_controls1),
+                                                                            n_controls2, len(reliable_controls2)))
 
-print()
+# Inspect unreliable controls
+if INSPECT_FAILED_CONTROLS:
+    print("\nFailed controls:")
+    scores_per_name['min_correct'] = scores_per_name[['correct1', 'correct2']].min(axis=1)
+    scores_per_name = scores_per_name.loc[~scores_per_name['min_correct'].isna()]
+    scores_per_name.sort_values(by='min_correct', inplace=True)
+    for c, control in scores_per_name.iterrows():
+        print("{} \"{}\" ({})\n  {}: {}, {}\n  {}: {}\n".format(
+              control['image_url'],
+              control['name'],
+              control['control_type'],
+              control['correct1'],
+              control['rating'],
+              control['type'],
+              control['correct2'],
+              control['same_color']))
 
-annotations['rating'] = annotations['rating'].apply(lambda x: (2 - x) / 2)    # mapping 2 to 0, 1 to 1/2, 0 to 1.
+# Inspect low-agreement items (lowest-agreement first)
 
-print("Per name:", len(annotations))
+
+print("\nPer name:", len(annotations))
 print(annotations[:10].to_string())
 
 
+# Create assignments dataframe with some basic stats:
 assignments = annotations.groupby(['assignmentid', 'workerid']).agg({'rating': 'mean', 'color': 'mean', 'correct1': ['count', 'sum'], 'correct2': ['count', 'sum']})
+
+# Compute control score:
 assignments.columns = ['_'.join(tup).rstrip('_') for tup in assignments.columns.values]
-
-assignments['n_controls'] = assignments[['correct1_count', 'correct2_count']].sum(axis=1)
 assignments['control_score'] = (assignments['correct1_sum'] + assignments['correct2_sum']) / (assignments['correct1_count'] + assignments['correct2_count'])
+assignments.drop(['correct1_count', 'correct1_sum', 'correct2_count', 'correct2_sum'], axis=1, inplace=True)
 
+# Aadd the control score according to M-turk:
 assignments = pd.merge(assignments, assignments_from_mturk[['assignmentid', 'control_score_mturk', 'workerid']], on='assignmentid')
 assignments['control_score_mturk'] = assignments['control_score_mturk'].apply(lambda x: 1 - float(x.split(',')[-1]))
 
-del assignments['correct1_count']
-del assignments['correct1_sum']
-del assignments['correct2_count']
-del assignments['correct2_sum']
 
-assignments['decision1'] = assignments['control_score'].apply(lambda x: "approve" if x >= APPROVAL_TRESHOLD else "reject")
+# Make decision and add bookkeeping columns to csv
+assignments['decision1'] = assignments['control_score'].apply(lambda x: "approve" if (x >= APPROVAL_TRESHOLD or NO_REJECTION) else "reject")
 assignments['decision2'] = assignments['control_score'].apply(lambda x: "bonus" if x >= BONUS_THRESHOLD else "block" if x < BLOCK_THRESHOLD else np.nan)
 assignments['executed1'] = False
 assignments['executed2'] = False
 
-# Add column with reliable mistakes (URL + control_type + rating/type/samecolor)
+# More bookkeeping: column with (reliable) mistakes (URL + control_type + rating/type/samecolor)
 assignments['mistakes'] = [[] for _ in range(len(assignments))]
 for i, row in assignments.iterrows():
     mistakes1 = annotations.loc[(annotations['assignmentid'] == row['assignmentid']) & (annotations['correct1'] == False)][['image_url', 'name', 'rating', 'type']].values.tolist()
@@ -248,39 +274,36 @@ for i, row in assignments.iterrows():
     assignments.at[i, 'mistakes'] = mistakes1 + mistakes2
 assignments['n_mistakes'] = assignments['mistakes'].apply(len)
 
+
+print()
 print(assignments.to_string())
 
-with open(os.path.join(resultsdir, 'assignments.csv'), 'w+') as outfile:
-    assignments.to_csv(outfile, index=False)
-    print("Assignments written to", outfile.name)
-
-# TODO compare quality scores to those in the MTurk field (merge from the original per_HIT data).
-
-
-print("Total: {} assignments. Approve: {} (bonus: {}). Reject: {} (block: {})".format(len(assignments),
+print("\nTotal: {} assignments. Approve: {} (bonus: {}). Reject: {} (block: {})".format(len(assignments),
                                                                                       len(assignments.loc[assignments['decision1'] == 'approve']),
                                                                                       len(assignments.loc[assignments['decision2'] == 'bonus']),
                                                                                       len(assignments.loc[assignments['decision1'] == 'reject']),
                                                                                       len(assignments.loc[assignments['decision2'] == 'block']),
                                                                                       ))
 
-print()
 
-print(annotations.groupby(['workerid', 'hitid'])['rating', 'correct1', 'correct2'].agg(['count', 'mean']).to_string())
+with open(os.path.join(resultsdir, 'per_assignment.csv'), 'w+') as outfile:
+    assignments.to_csv(outfile, index=False)
+    print("\nAssignments written to", outfile.name)
 
-# print(per_name.loc[per_name['control_type'] == 'alternative'].to_string())
-
-
-# print(per_name.loc[per_name['control_type'].apply(lambda x: x.startswith('typo'))].to_string())
 
 if ANONYMIZE:
     for i, workerid in enumerate(annotations['workerid'].unique()):
         annotations.replace(to_replace={'workerid': {workerid: 'worker{}'.format(i)}}, inplace=True)
+    print("Worker IDs anonymized.")
 
-# per_name['correct2'] = per_name['correct2'].astype(bool) ## TODO Meh this doesn't work.
 
-
-with open(os.path.join(resultsdir, 'names.csv'), 'w+') as outfile:
+with open(os.path.join(resultsdir, 'per_name.csv'), 'w+') as outfile:
     annotations.to_csv(outfile)
     print("Answers per name written to", outfile.name)
+
+
+# # For quick inspection
+# print()
+# print(annotations.groupby(['workerid', 'hitid'])['rating', 'correct1', 'correct2'].agg(['count', 'mean']).to_string())
+
 
