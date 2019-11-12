f1893aa62d99b8a2f0a30453f0015d9105b33bcd
matthijswestera <matthijs.westera@gmail.com>
2019-11-12 15:56:11 +0100

    *sigh* MAX_IMAGES and MAX_NAMES set too low; was not showing all data

--- verification_phase0/results_to_csv--complex.py
+++ verification_phase0/results_to_csv--complex.py
@@ -62,8 +62,8 @@ assignments_from_mturk = pd.DataFrame(assignments_from_mturk)
 assignments_from_mturk.columns = [x.lower() for x in assignments_from_mturk.columns]
 assignments_from_mturk.rename(columns={"stored_font_size": 'control_score_mturk'}, inplace=True)
 
-MAX_N_IMAGES = max([int(col.split('-')[0][3:]) for col in assignments_from_mturk.columns if col.startswith('img')])
-MAX_N_NAMES = max([int(col.split('-')[1][4:]) for col in assignments_from_mturk.columns if col.startswith('img') and col.split('-')[1].startswith('name')])
+MAX_N_IMAGES = max([int(col.split('-')[0][3:]) for col in assignments_from_mturk.columns if col.startswith('img')]) + 1
+MAX_N_NAMES = max([int(col.split('-')[1][4:]) for col in assignments_from_mturk.columns if col.startswith('img') and col.split('-')[1].startswith('name')]) + 1
 
 # Merge boolean radiobutton columns into integer-valued rating columns
 for i in range(MAX_N_IMAGES):
@@ -104,9 +104,9 @@ for i in reversed(range(MAX_N_IMAGES)): # reversed in order for names with 10 an
     columns = meta + ['items'] + [col for col in assignments_from_mturk.columns if col.startswith('img{}'.format(i))]
     df = assignments_from_mturk[columns].copy()
     df.columns = [col.replace('img{}-'.format(i), '').lower() for col in df.columns]
-    df['names'] = df['items'].apply(lambda x: x[i][1] if len(x) > i else np.nan)
+    df['names'] = df['items'].apply(lambda x: x[i][1] if len(x) > i else [])
     df['n_names'] = df['names'].apply(len)
-    df['image_url'] = df['items'].apply(lambda x: x[i][0] if len(x) > i else np.nan)
+    df['image_url'] = df['items'].apply(lambda x: x[i][0] if len(x) > i else '')
     # unobfuscate quality control
     df['quality_control'] = df['items'].apply(lambda x: json.loads(bytes.fromhex(x[i][2]).decode('utf-8')) if len(x) > i else {})
     del df['items']
@@ -185,10 +185,10 @@ name_annotations['correct2'] = np.nan
 name_annotations['correct3'] = np.nan
 name_annotations['correct3_explanation'] = ""
 for i, row in name_annotations.iterrows():
-    if row['control_type'] == 'vg_majority':
+    if row['control_type'] == 'vg_majority':    # max 1 point
         name_annotations.at[i, 'correct1'] = float(row['rating'] == 0)
 
-    elif row['control_type'].startswith('typo'):
+    elif row['control_type'].startswith('typo'):  # 2 points
         original = '-'.join(row['control_type'].split('-')[1:])
         original_row = name_annotations.loc[name_annotations['assignmentid'] == row['assignmentid']].loc[(name_annotations['image'] == row['image']) & (name_annotations['object'] == row['object'])].loc[name_annotations['name'] == original].squeeze()
         if row['rating'] == 0:
@@ -202,35 +202,39 @@ for i, row in name_annotations.iterrows():
             name_annotations.at[i, 'correct1'] = float(True)
         name_annotations.at[i, 'correct2'] = float(original in row['same_color'])
 
-    elif row['control_type'] == 'alternative':
+    elif row['control_type'] == 'alternative':  # 1; 2 if pos
         name_annotations.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] not in ['bounding box', 'other']))
         positive = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['image'] == row['image']) & (name_annotations['object'] == row['object']) & (name_annotations['control_type'] == 'vg_majority')]
         if len(positive) > 0:
             name_annotations.at[i, 'correct2'] = float(positive['name'].squeeze() not in row['same_color'])
 
-    elif row['control_type'] == 'random':
+    elif row['control_type'] == 'random':  # 2 points
         name_annotations.at[i, 'correct1'] = float(not (row['rating'] == 0 or row['type'] != 'other'))
         name_annotations.at[i, 'correct2'] = float(len(row['same_color']) == 1)
 
-    elif row['control_type'].startswith('syn'):
+    elif row['control_type'].startswith('syn'): # 2 points
         synonyms = row['control_type'][4:].split(',')
         same_rating = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['image'] == row['image']) & (name_annotations['object'] == row['object']) & (name_annotations['rating'] == row['rating'])]['name'].unique().tolist()
+        same_type = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['image'] == row['image']) & (name_annotations['object'] == row['object']) & (name_annotations['type'] == row['type'])]['name'].unique().tolist()
         name_annotations.at[i, 'correct1'] = float(not any([syn not in same_rating for syn in synonyms]))
+        if row['rating'] != 0 and name_annotations.at[i, 'correct1'] == 1:
+            name_annotations.at[i, 'correct1'] = float(not any([syn not in same_type for syn in synonyms]))
         name_annotations.at[i, 'correct2'] = float(not any([syn not in row['same_color'] for syn in synonyms]))
 
     else: # not a control item, but still a consistency check possible:
         if row['rating'] == 2 and row['type'] == 'bounding box':
             names_deemed_good = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['image'] == row['image']) & (name_annotations['object'] == row['object']) & (name_annotations['rating'] == 0)]['name'].unique().tolist()
-            good_names_same_color = [n for n in row['same_color'] if n in names_deemed_good]
-            name_annotations.at[i, 'correct3'] = float(len(good_names_same_color) == 0)
-            if not name_annotations.at[i, 'correct3']:
-                name_annotations.at[i, 'correct3_explanation'] = row['name'] + '_' + ','.join(good_names_same_color)
+            if len(names_deemed_good) > 0:
+                good_names_same_color = [n for n in row['same_color'] if n in names_deemed_good]
+                name_annotations.at[i, 'correct3'] = float(len(good_names_same_color) == 0)
+                if not name_annotations.at[i, 'correct3']:
+                    name_annotations.at[i, 'correct3_explanation'] = row['name'] + '_' + ','.join(good_names_same_color)
 
 # Change how rating is represented; this is a bit risky, but it works, so let's not touch it.
 # name_annotations['rating'] = name_annotations['rating'].apply(lambda x: (2 - x) / 2)    # mapping 2 to 0, 1 to 1/2, 0 to 1.
 
 # Compute which controls are reliable (>X% correct)
-scores_per_name = name_annotations.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean', 'correct3': 'mean', 'image_url': lambda x: x.tolist()[0], 'control_type': lambda x: x.tolist()[0], 'rating': lambda x: x.tolist(), 'type': lambda x: x.tolist(), 'same_color': lambda x: x.tolist(), 'control_type': lambda x: x.to_list()[0]}).reset_index()
+scores_per_name = name_annotations.groupby(['image', 'object', 'name']).agg({'correct1': 'mean', 'correct2': 'mean', 'correct3': 'mean', 'image_url': lambda x: x.tolist()[0], 'control_type': lambda x: x.tolist()[0], 'rating': lambda x: x.tolist(), 'type': lambda x: x.tolist(), 'same_color': lambda x: x.tolist()}).reset_index()
 scores_per_name['reliable1'] = scores_per_name['correct1'] >= CONTROL_RELIABILITY_THRESHOLD
 scores_per_name['reliable2'] = scores_per_name['correct2'] >= CONTROL_RELIABILITY_THRESHOLD
 reliable1_controls = scores_per_name.loc[scores_per_name['reliable1']][['image', 'object', 'name']].values.tolist()
@@ -281,19 +285,23 @@ name_annotations.at[~name_annotations['reliable2'], 'correct2-filtered'] = np.na
 
 # Create assignments dataframe with some basic stats: # TODO Oh my this is ugly code...
 assignments = name_annotations.groupby(['hitid', 'requesterannotation', 'assignmentid', 'workerid']).agg({'rating': 'mean', 'color': 'mean',
-                                                                                                     'correct1': ['count', 'sum'], 'correct2': ['count', 'sum'], 'correct3': ['count', 'sum'],
+                                                                                                     'correct1': ['count', 'sum'], 'correct2': ['count', 'sum'], 'correct3': ['count', 'sum', lambda x: x.tolist()],
                                                                                                      'correct1-filtered': ['count', 'sum'], 'correct2-filtered': ['count', 'sum'], 'correct3-filtered': ['count', 'sum'], })
 assignments.columns = ['_'.join(tup).rstrip('_') for tup in assignments.columns.values]
 assignments['control_score'] = (assignments['correct1_sum'] + assignments['correct2_sum'] + assignments['correct3_sum']) / (assignments['correct1_count'] + assignments['correct2_count'] + assignments['correct3_count'])
 assignments['control_score-filtered'] = (assignments['correct1-filtered_sum'] + assignments['correct2-filtered_sum'] + assignments['correct3-filtered_sum']) / (assignments['correct1-filtered_count'] + assignments['correct2-filtered_count'] + assignments['correct3-filtered_count'])
-assignments.drop(['correct1_count', 'correct1_sum', 'correct2_count', 'correct2_sum', 'correct3_count', 'correct3_sum'], axis=1, inplace=True)
+assignments['n_controls'] = assignments['correct1_count'] + assignments['correct2_count'] + assignments['correct3_count']
+assignments['n_correct'] = assignments['correct1_sum'] + assignments['correct2_sum'] + assignments['correct3_sum']
+# assignments.drop(['correct1_count', 'correct1_sum', 'correct2_count', 'correct2_sum', 'correct3_count', 'correct3_sum'], axis=1, inplace=True)
 assignments.drop(['correct1-filtered_count', 'correct1-filtered_sum', 'correct2-filtered_count', 'correct2-filtered_sum', 'correct3-filtered_count', 'correct3-filtered_sum'], axis=1, inplace=True)
 
 
 # Add the control score according to M-turk:
 assignments = pd.merge(assignments, assignments_from_mturk[['hitid', 'requesterannotation', 'assignmentid', 'control_score_mturk', 'workerid']], on='assignmentid')
+assignments['errors_mturk'] = assignments['control_score_mturk'].apply(lambda x: x.split(',')[-1].replace('serif', 'alt').replace('courier', 'rand').replace('arial', 'pos').replace('sans', 'typo').split('_'))
+assignments['n_controls_mturk'] = assignments['control_score_mturk'].apply(lambda x: int(x.split(',')[1]))
+assignments['n_correct_mturk'] = assignments['n_controls_mturk'] - assignments['control_score_mturk'].apply(lambda x: int(x.split(',')[0]))
 assignments['control_score_mturk'] = assignments['control_score_mturk'].apply(lambda x: 1 - float(x.split(',')[2]))
-
 
 # Make decision and add bookkeeping columns to csv
 assignments['decision1'] = ""
@@ -345,15 +353,24 @@ workers_to_ignore = set(workers_to_ignore)
 # More bookkeeping: column with mistakes (URL + control_type + rating/type/samecolor)
 assignments['mistakes'] = [[] for _ in range(len(assignments))]
 for i, row in assignments.iterrows():
-    mistakes1 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct1'] == False)][['image_url', 'name', 'rating', 'type']].values.tolist()
-    mistakes2 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct2'] == False)][['image_url', 'name', 'same_color']].values.tolist()
-    mistakes3 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct3'] == False)][['image_url', 'name', 'correct3_explanation']].values.tolist()
-    mistakes1 = ["{} - {}: {}".format(m[0], m[1], m[2] if m[2] == 0 else m[3]) for m in mistakes1]
-    mistakes2 = ["{} - {}: {}".format(m[0], m[1], m[2]) for m in mistakes2]
-    mistakes3 = ["{} - {} ~ {}".format(m[0], m[1], m[2]) for m in mistakes3]
+    mistakes1 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct1'] == 0.0)][['image_url', 'name', 'control_type', 'rating', 'type']].values.tolist()
+    mistakes2 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct2'] == 0.0)][['image_url', 'name', 'control_type', 'same_color']].values.tolist()
+    mistakes3 = name_annotations.loc[(name_annotations['assignmentid'] == row['assignmentid']) & (name_annotations['correct3'] == 0.0)][['image_url', 'name', 'control_type', 'correct3_explanation']].values.tolist()
+    mistakes1 = ["{} - {} - {}: {}".format(m[0], m[1], m[2], m[3] if m[3] == 0 else m[4]) for m in mistakes1]
+    mistakes2 = ["{} - {} - {}: {}".format(m[0], m[1], m[2], m[3]) for m in mistakes2]
+    mistakes3 = ["{} - {} - {} ~ {}".format(m[0], m[1], m[2], m[3]) for m in mistakes3]
     assignments.at[i, 'mistakes'] = mistakes1 + mistakes2 + mistakes3
 assignments['n_mistakes'] = assignments['mistakes'].apply(len)
 
+for i, row in assignments.iterrows():
+    if abs(row['control_score_mturk'] - row['control_score']) > 0.01:
+        print("WARNING: control_score from mturk and from python are different!", i)
+        # print(row['assignmentid'], row['control_score'], '!=', row['control_score_mturk'])
+        # print(row['n_correct'], '/', row['n_controls'], ';', row['n_correct_mturk'], '/', row['n_controls_mturk'])
+        # print(row['correct1_count'], row['correct2_count'], row['correct3_count'])
+        # print('  ', row['errors_mturk'])
+        # print('  ', '\n  '.join(row['mistakes']))
+        # input()
 
 print('\n---------------')
 print(assignments[:5].to_string())
