bc5df818e56da231a30e20b2c687c386001e1034
matthijswestera <matthijs.westera@gmail.com>
2020-04-08 14:19:23 +0200

    add krippendorff agreement

--- analysis/analysis_verification_matthijs.py
+++ analysis/analysis_verification_matthijs.py
@@ -9,13 +9,17 @@ from tqdm import tqdm
 from functools import reduce
 from operator import mul
 from math import factorial as fac
-
+import krippendorff
 import seaborn as sns
 
 from analysis import load_results
 
 import random
 import statistics
+
+from scipy.stats import spearmanr
+
+import math
 
 random.seed(12345)
 np.random.seed(129387)
@@ -150,7 +154,69 @@ def basic_stats():
 
 
 
-def analytic(auxfile):
+def basic_stats_old():
+    """
+    Originally from merge_and_clean_csv.py
+    :return:
+    """
+
+    annotations_all = pd.read_csv("name_annotations_filtered_ANON.csv")
+
+    annotations_targets = annotations_all.loc[
+        annotations_all['control_type'].isna() | (annotations_all['control_type'] == 'vg_majority') | (
+            annotations_all['control_type'].apply(lambda x: isinstance(x, str) and x.startswith('syn')))]
+
+    print("\n-------------")
+    print(annotations_all[:5].to_string())
+    print('name_annotations:', len(annotations_all))
+    print('targets:', len(annotations_targets))
+    print('unique images:', len(annotations_all['image'].unique()), "/", len(annotations_targets['image'].unique()))
+    print('unique names:', len(annotations_all['name'].unique()), "/", len(annotations_targets['name'].unique()))
+    print("-------------")
+
+    print("\n~~~~~~~~~~~~~~")
+    print("Inter-annotator agreement:")
+    scores_per_name = annotations_targets.groupby(['image', 'object', 'name']).agg({'adequacy': lambda x: x.tolist(), 'inadequacy_type': lambda x: x.tolist(), 'name_cluster': lambda x: x.tolist(), 'same_object': lambda x: x.tolist()}).reset_index()
+    # Remove points with <=1 annotator to avoid division by zero
+    scores_per_name = scores_per_name.loc[scores_per_name['adequacy'].apply(lambda x: len(x) > 1)]
+    # Are these stats biased towards images with more names? They are counted more times... Then again, there are more names there to agree or not agree on.
+    scores_per_name['adequacy_bin'] = scores_per_name['adequacy'].apply(lambda x: [0 if a == 0 else 1 for a in x])
+    scores_per_name['inadequacy_type'] = scores_per_name['inadequacy_type'].apply(lambda x: [str(t) for t in x])
+    scores_per_name['adequacy_agreement_cat'] = scores_per_name['adequacy'].apply(lambda x: sum([(x.count(score) * (x.count(score) - 1)) / (len(x) * (len(x) - 1)) for score in [0, 1, 2]]))
+    scores_per_name['adequacy_agreement_bin'] = scores_per_name['adequacy_bin'].apply(lambda x: sum([(x.count(score) * (x.count(score) - 1)) / (len(x) * (len(x) - 1)) for score in [0, 1]]))
+    scores_per_name['adequacy_agreement_std'] = scores_per_name['adequacy'].apply(np.std)
+    scores_per_name['inadequacy_type_agreement'] = scores_per_name['inadequacy_type'].apply(lambda x: sum([(x.count(score) * (x.count(score) - 1)) / (len(x) * (len(x) - 1)) for score in['nan', 'linguistic', 'bounding box', 'visual', 'other']]))
+    scores_per_name['same_object'] = scores_per_name['same_object'].apply(lambda x: {key: [d[key] for d in x] for key in x[0]})
+    scores_per_name['same_object_agreement'] = scores_per_name['same_object'].apply(lambda x: [sum([(x[k].count(score) * (x[k].count(score) - 1)) / (len(x[k]) * (len(x[k]) - 1)) for score in [0, 1]]) for k in x])
+    scores_per_name['same_object_agreement'] = scores_per_name['same_object_agreement'].apply(lambda x: sum(x) / len(x))
+    print(scores_per_name[['adequacy_agreement_cat', 'adequacy_agreement_bin', 'adequacy_agreement_std', 'inadequacy_type_agreement', 'same_object_agreement']].mean())
+    print("~~~~~~~~~~~~~~")
+    print()
+
+    ## Aggregate multiple annotations for a image+name:
+    # adequacy: mean
+    # inadequacy_type: majority, otherwise 'unknown'
+    # same_object: majority, otherwise 'unknown'
+    scores_per_name['adequacy_mean'] = scores_per_name['adequacy'].apply(lambda x: sum(x)/len(x))
+    scores_per_name['inadequacy_type_majority'] = scores_per_name['inadequacy_type'].apply(lambda x: Counter(x).most_common(1)[0][0]) # TODO What if no option has majority?
+    scores_per_name['same_object_majority'] = scores_per_name['same_object'].apply(lambda x: {k: Counter(x[k]).most_common(1)[0][0] for k in x})
+    scores_per_name['name_cluster_majority'] = scores_per_name['same_object_majority'].apply(lambda x: tuple(sorted(k for k in x if x[k] == 1)))
+    # print(scores_per_name[:20].to_string())
+
+    print("Number of names total:", len(scores_per_name))
+    print("Number of adequate names total:", len(scores_per_name.loc[scores_per_name['adequacy_mean'] < 1]))
+    print("Adequate names per image (<1):", scores_per_name.loc[scores_per_name['adequacy_mean'] < 1].groupby('image').agg({'name': 'count'}).mean().values[0])
+    print("Clusters (majority) per image:", scores_per_name.groupby('image').agg({'name_cluster_majority': 'nunique'}).mean().values[0])
+    print("Adequate clusters (majority) per image:", scores_per_name.loc[scores_per_name['adequacy_mean'] < 1].groupby('image').agg({'name_cluster_majority': 'nunique'}).mean().values[0])
+    cluster_sizes = scores_per_name.groupby('image').agg({'name_cluster_majority': lambda x: [len(t) for t in x.unique()]})
+    cluster_sizes_flat = [x for y in cluster_sizes['name_cluster_majority'].tolist() for x in y]
+    print("Cluster size (majority):", sum(cluster_sizes_flat)/len(cluster_sizes_flat))
+    cluster_sizes = scores_per_name.loc[scores_per_name['adequacy_mean'] < 1].groupby('image').agg({'name_cluster_majority': lambda x: [len(t) for t in x.unique()]})
+    cluster_sizes_flat = [x for y in cluster_sizes['name_cluster_majority'].tolist() for x in y]
+    print("Adequate cluster size (majority):", sum(cluster_sizes_flat)/len(cluster_sizes_flat))
+
+
+def compute_stability_analytic(auxfile):
 
     settings = ['all_names', 'entry_cluster_names']
 
@@ -222,7 +288,7 @@ def analytic(auxfile):
     plt.show()
 
 
-def stochastic(auxfile):
+def compute_stability_stochastic(auxfile):
 
     settings = ['all names', 'adequate names', 'canonical object names', 'adequate canonical names']
 
@@ -409,16 +475,129 @@ def n_names_for_prob_majority(probs, threshold=.9, max_n_names=38, increment=2):
     return 99
 
 
+def adequacy_against_frequency():
+    csvfile = '../proc_data_phase0/verification/all_responses_round0-3_verified.csv'
+    df = load_results.load_cleaned_results(csvfile)
+
+    datapoints = []
+    for i, row in tqdm(df.iterrows(), total=len(df)):
+        freqs = row['spellchecked_min2'].values()
+        total = sum(freqs)
+        max_freq = max(freqs) / total
+        for name in row['verified']:
+            freq = row['spellchecked_min2'][name] / total
+            adeq = row['verified'][name]['adequacy']
+            top = "top" if freq == max_freq else "other"
+
+            # freq = math.ceil(10*(row['spellchecked_min2'][name]/total))/10
+            # adeq = math.ceil(6*row['verified'][name]['adequacy'])/6
+            # Merge the first into a bin
+            # freq = max(.1, freq)
+            # adeq = max(1/6, adeq)
+
+            if freq != 0:
+                datapoints.append([freq, adeq, top, 1])
+
+    datapoints = pd.DataFrame(datapoints, columns=["frequency", "adequacy", "name", "count"])
+
+    # Heatmap attempt 1:
+    if False:
+        datapoints = datapoints.groupby(["frequency", "adequacy"]).count().unstack(level=-1)['count']
+
+        ax = sns.heatmap(datapoints, square=True)
+        ax.set_ylim(0, 10)  # to fix bug due to matplotlib
+        ax.set_yticklabels(ax.get_yticklabels())  # Reset to remove rotation
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label1.set_horizontalalignment('left')
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label1.set_verticalalignment('top')
+
+        plt.show()
+
+    # Heatmap attempt 2:
+    if False:
+        plt.hist2d(data=datapoints, x="adequacy", y="frequency", bins=[7, 36])
+        plt.show()
+
+    print(spearmanr(datapoints['frequency'], datapoints['adequacy']))
+
+    # Scatterplot:
+    if True:
+        sns.lmplot(data=datapoints, x='frequency', y='adequacy', hue='name', x_jitter=.03, y_jitter=.07, truncate=True,
+                   scatter_kws={'alpha': .05})
+        plt.show()
+
+
+def inter_rater_agreement():
+    verification_data_path = '../verification_phase0/1_crowdsourced/results/merged_1-2-3-4-5-6-7-8_redone/name_annotations_filtered_ANON.csv'
+    verifications = pd.read_csv(verification_data_path, converters={'name_cluster-nofillers': eval, 'same_object-nofillers': eval})
+    del verifications['batch']
+    del verifications['source']
+    del verifications['hitid']
+    del verifications['assignmentid']
+    del verifications['name_cluster']
+    del verifications['same_object']
+
+    # Remove all manipulated control items
+    # ad hoc fix for weird bug: elizabethville is not in fact a control item
+    verifications = verifications.loc[(verifications['name'] == 'elizabethville') |
+                                      verifications['control_type'].isna() |
+                                      (verifications['control_type'] == 'vg_majority') |
+                                      (verifications['control_type'].apply(
+                                          lambda x: isinstance(x, str) and x.startswith('syn')))]
+    verifications.rename(columns={'name_cluster-nofillers': 'name_cluster', 'same_object-nofillers': 'same_object'},
+                         inplace=True)
+
+    verifications['inadequacy_type'] = verifications['inadequacy_type'].apply(lambda x: "none" if pd.isna(x) else x)
+
+    replacements = {'none': 0, 'linguistic': 1, 'bounding box': 2, 'visual': 3, 'other': 4}
+    verifications['inadequacy_type_num'] = verifications['inadequacy_type'].apply(lambda x: replacements[x])
+
+    # TODO I don't understand why there are duplicates...
+    verifications = verifications[~verifications.duplicated(['workerid', 'image', 'object', 'name'], keep='first')]
+
+    print(verifications[:5].to_string())
+
+    # # Computing adequacy agreement
+    verifications['unit'] = verifications.apply(lambda x: f"{x['image']}-{x['object']}-{x['name']}", axis=1)
+    # reliability_data = verifications[['workerid', 'unit', 'adequacy']].pivot(index='workerid', columns='unit')
+    # print("Adequacy: Krippendorff's alpha (ordinal) =", krippendorff.alpha(reliability_data, level_of_measurement='ordinal'))
+    #
+    # # computing inadequacy type agreement
+    # reliability_data = verifications[['workerid', 'unit', 'inadequacy_type_num']].pivot(index='workerid', columns='unit')
+    # print("Inadequacy type: Krippendorff's alpha (nominal) =", krippendorff.alpha(reliability_data, level_of_measurement='nominal'))
+
+    # computing same-object agreement
+    new_rows = []
+    for i, row in tqdm(verifications.iterrows()):
+        for key in row['same_object']:
+            new_rows.append([row['workerid'], row['unit'] + '-' + key, row['same_object'][key]])
+    verifications_per_name_pair = pd.DataFrame(new_rows, columns=["workerid", "unit", "same_object"])
+    reliability_data = verifications_per_name_pair[['workerid', 'unit', 'same_object']].pivot(index='workerid', columns='unit')
+    print("Same_object: Krippendorff's alpha (nominal) =", krippendorff.alpha(reliability_data, level_of_measurement='nominal'))
+
+
 if __name__ == "__main__":
 
-    do_analytic = not input("Analytic or stochastic? A/s").lower().startswith('s')
+    if False:
+        basic_stats()
 
-    auxfile = 'aux/stability_samples_{}_{}_{}_{}{}'.format(ADEQUACY_THRESHOLD, CANONICAL_THRESHOLD, NUM_SAMPLES_FOR_STABILITY, SUBSAMPLE, "_analytic" if do_analytic else "")
-    os.makedirs('aux', exist_ok=True)
+    if True:
+        inter_rater_agreement()
+        # Adequacy: Krippendorff's alpha (ordinal) = 0.7067100617783595
+        # Inadequacy type: Krippendorff's alpha (nominal) = 0.6734359534740075
+        # Same-object: Krippendorff's alpha (nominal) = 0.8332111041592695
 
-    basic_stats()
+    if False:
+        adequacy_against_frequency()
 
-    if do_analytic:
-        analytic(auxfile)
-    else:
-        stochastic(auxfile)
+    if False:
+        do_analytic = not input("Analytic or stochastic? A/s").lower().startswith('s')
+
+        auxfile = 'aux/stability_samples_{}_{}_{}_{}{}'.format(ADEQUACY_THRESHOLD, CANONICAL_THRESHOLD, NUM_SAMPLES_FOR_STABILITY, SUBSAMPLE, "_analytic" if do_analytic else "")
+        os.makedirs('aux', exist_ok=True)
+
+        if do_analytic:
+            compute_stability_analytic(auxfile)
+        else:
+            compute_stability_stochastic(auxfile)
